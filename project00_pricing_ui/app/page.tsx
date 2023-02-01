35f60132cd1c1f0edc68cf46268cdf3c346862cc
Hassan-Mehmood <hassanafridi1913@gmail.com>
2023-02-01 15:59:30 +0500

    working on pricing ui

--- project00_pricing_ui/app/page.tsx
+++ project00_pricing_ui/app/page.tsx
@@ -1,10 +1,23 @@
 'use client';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({
+  subsets: [],
+  weight: '800',
+});
+
 export default function Home() {
   return (
     <ChakraProvider>
-      <Box>Hellp</Box>
+      <Box as="main">
+        {/* Purple Box*/}
+        <Box px="1rem" py="2.75rem" bgColor="#6B46C1" textColor="#fff">
+          <Heading className={inter.className}>Simple pricing for your business</Heading>
+          <Text>Plans that are carefully crafted to suit your business</Text>
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
