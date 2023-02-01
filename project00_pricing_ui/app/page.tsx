bc9b0e6cf4644ea2231062f3f84e46777804aa9a
Hassan-Mehmood <hassanafridi1913@gmail.com>
2023-02-01 15:27:12 +0500

    Creating the pricing ui

---
+++ project00_pricing_ui/app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Box } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+export default function Home() {
+  return (
+    <ChakraProvider>
+      <Box>Hellp</Box>
+    </ChakraProvider>
+  );
+}
